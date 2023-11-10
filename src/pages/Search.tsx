78effe3bb898c7c3fca03980166ff0aa5ab8b4e1
Munch <mphlife@gmail.com>
2023-11-09 16:10:25 -1000

    filtering change

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import dynamic from "next/dynamic";
 import "leaflet/dist/leaflet.css";
 import type { InsuranceProviders } from "~/server/api/routers/HealthcareRouter";
+import type { ProcedureProviders } from "~/server/api/routers/HealthcareRouter";
 import { api } from "~/utils/api";
 import Navbar from "~/components/Navbar";
 import { LoadingSpinner } from "~/components/Loading";
@@ -13,15 +14,14 @@ const SearchPage: React.FC = () => {
   // this is used for filtering.
   // allow user to set insurance status (QUEST, none, or both)
   const [insurance, setInsurance] = useState<InsuranceProviders | undefined>();
+  const [procedure, setProcedure] = useState<ProcedureProviders | undefined>();
 
   // if no insurance selected, select first 100 clinics.
   // if there is, plug it into our api.
   // this really needs to be one procedure with the insurance being optional.
   // this is unsorted. it is sorted however mongodb wants to sort it :)
-  const { data: centers, isLoading } =
-    insurance !== undefined
-      ? api.healthcare.getByPlan.useQuery({ insurance })
-      : api.healthcare.getSome.useQuery();
+
+  const {data: insuranceCenters, isLoading: insuranceIsLoading} = api.healthcare.getByPlan.useQuery({ insurance, procedure });
 
   return (
     <div className="h-screen overflow-hidden font-tyler">
@@ -49,6 +49,27 @@ const SearchPage: React.FC = () => {
               <option onClick={(_p) => setInsurance(undefined)}>Any</option>
             </select>
           </div>
+          <div className="form-control w-full px-6">
+            <label className="label">
+              <span className="label-text text-base font-semibold">
+                Procedure type
+              </span>
+            </label>
+            <select className="select select-bordered text-base" defaultValue={"Pick one"}>
+              <option disabled>
+                Pick one
+              </option>
+              <option onClick={(p) => setProcedure("Comprehensive Care")}>Comprehensive Care</option>
+              <option onClick={(p) => setProcedure("Obstetrician and Gynecologist")}>Obstetrician and Gynecologist</option>
+              <option onClick={(p) => setProcedure("Internal Medicine")}>Internal Medicine</option>
+              <option onClick={(p) => setProcedure("RN Family Nurse Practitioner")}>RN Family Nurse Practitioner</option>
+              <option onClick={(p) => setProcedure("Family Medicine")}>Family Medicine</option>
+              <option onClick={(p) => setProcedure("Pediatrician")}>Pediatrician</option>
+              <option onClick={(p) => setProcedure("Podiatrist")}>Podiatrist</option>
+              <option onClick={(p) => setProcedure("Nephrologist")}>Nephrologist</option>
+              <option onClick={(p) => setProcedure(undefined)}>Any</option>
+            </select>
+          </div>
         </div>
         <div
           className="0 h-full w-1/2 flex-col overflow-y-scroll
@@ -61,12 +82,12 @@ const SearchPage: React.FC = () => {
                     DO NOT USE STATIC SITE GENERATION BECAUSE YOUTUBE DOESN'T DO THIS WHEN YOU SEARCH FOR VIDEOS
                     then, when we get the data we can map it out into a DIV for each center. rn we are missing a more info <button className=""></button>
                     */}
-          {isLoading ? (
+          {(insuranceIsLoading || !insuranceCenters) ? (
             <div className="mt-12 grid justify-items-center justify-self-center">
               <LoadingSpinner />
             </div>
           ) : (
-            centers?.map((c) => (
+            insuranceCenters.map((c) => (
               <div
                 className={
                   "mx-12 mb-6 rounded-xl border-2 border-gray-100 bg-gray-100 p-4"
@@ -81,7 +102,13 @@ const SearchPage: React.FC = () => {
                         ? "text-dark-blue"
                         : "text-dark-blue"
                     }
+                    
                   >
+                  {/* <a>
+                    className={
+                      c.procedureTypeNames.includes("")
+                    }
+                  </a> */}
                     {c.names[0] + " (" + (false || "Comprehensive Care") + ")"}
                   </a>
                 </div>
@@ -91,11 +118,43 @@ const SearchPage: React.FC = () => {
                     {/* {isLoggedIn ? 'currently' : 'not'} */}
                     {c.website && <a className="italic underline text-blue-700" href={"https://" + c.website}>{c.website}</a>}
                     <div>{c.address}</div>
-                    {c.healthCenterNumbers[0] && <div>{(() => {
-                      const num = c.healthCenterNumbers[0];
-                      return '(' + num.slice(0, 3) + ") " + num.slice(3, 6) + '-' + num.slice(6);
-                    })()}</div>}
-                    <div>{c.supportedInsurances.includes("QI") && "Quest Insured"}{c.supportedInsurances.includes("FQHC") && "Federally Qualified Health Center"}</div>
+                    <div>{c.healthCenterNumbers}</div>
+                    <div>{c.supportedInsurances}</div>
+{/* `                    <div>{procedureTypeNames}<div>` */}
+                    <div className="[shape-outside:inset(calc(100% - 100px) 0 0)] float-right flex items-end">
+                      <button
+                        onClick={async (_) => {
+                          let loc = "";
+                          console.log(navigator.geolocation);
+                          if (navigator.geolocation) {
+                            loc = await new Promise((res) =>
+                              navigator.geolocation.getCurrentPosition(
+                                (p) =>
+                                  res(
+                                    p.coords.latitude +
+                                    "," +
+                                    p.coords.longitude,
+                                  ),
+                                (_err) => res(""),
+                                {
+                                  enableHighAccuracy: true,
+                                },
+                              ),
+                            );
+                          }
+                          window.open(
+                            "https://www.google.com/maps/dir/" +
+                            loc +
+                            "/" +
+                            c.address,
+                          );
+                        }}
+                        rel="noopener noreferrer"
+                        className="text-md btn h-12 w-36 border-0 bg-light-green p-2 font-bold text-green-gray hover:bg-hover-green"
+                      >
+                        Get Directions
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
--- src/server/api/routers/HealthcareRouter.ts
+++ src/server/api/routers/HealthcareRouter.ts
@@ -3,9 +3,11 @@ import { createTRPCRouter, publicProcedure } from "../trpc";
 import { z } from "zod";
 import type { Prisma } from "@prisma/client";
 
-const InsuranceValidator = z.union([z.literal("FQHC"), z.literal("QI")]);
+const InsuranceValidator = z.union([z.literal("FQHC"), z.literal("QI"), z.undefined()]);
+const ProcedureValidator = z.union([z.literal("Comprehensive Care"), z.literal("Obstetrician and Gynecologist"), z.literal("Internal Medicine"), z.literal("RN Family Nurse Practitioner"), z.literal("Family Medicine"), z.literal("Pediatrician"), z.literal("Podiatrist"), z.literal("Nephrologist"), z.undefined()]);
 
 export type InsuranceProviders = z.infer<typeof InsuranceValidator>;
+export type ProcedureProviders = z.infer<typeof ProcedureValidator>;
 export type IDToMeta = Record<string, { names: string[], address: string }>;
 
 const HealthcareRouter = createTRPCRouter({
@@ -24,23 +26,31 @@ const HealthcareRouter = createTRPCRouter({
     }),
   // must be FQHC or QI for 'quest insurance'
   getByPlan: publicProcedure
-    .input(z.object({ insurance: InsuranceValidator }))
+    .input(z.object({ insurance: InsuranceValidator, procedure: ProcedureValidator }))
     .query(async ({ input }) => {
-      const a = await db.healthCenter.findMany({
+      var supportedInsurances = {
+        isEmpty: false
+      }
+      if(input.insurance) supportedInsurances = {has: input.insurance};
+      var procedureTypeNames = {
+        isEmpty: false
+      }
+      if(input.procedure) procedureTypeNames = {has: input.procedure};
+      const a = (input.insurance || input.procedure) ? await db.healthCenter.findMany({
         where: {
-          supportedInsurances: {
-            has: input.insurance,
-          },
-        },
-        take: 100,
-      });
+          supportedInsurances,
+          AND: {
+            procedureTypeNames
+          }
+        }
+      }) : await db.healthCenter.findMany();
 
       return a;
+
     }),
+
   getSome: publicProcedure.query(async () => {
-    const a = await db.healthCenter.findMany({
-      take: 100,
-    });
+    const a = await db.healthCenter.findMany();
 
     return a;
   }),
@@ -99,24 +109,24 @@ const HealthcareRouter = createTRPCRouter({
     });
     return features;
   }),
-  getDataByName: publicProcedure.input(z.object({ name: z.string() })).query(async ({ input }) => {
-    const lol: ({_id: {$oid: string }} & Prisma.HealthCenterGroupByOutputType)[] = await db.healthCenter.findRaw({
-        filter: {
-            names: {
-                // prisma doesnt have an elegant way of filtering if a letter occurs in a string[] so we use regexes.
-                $regex: input.name.replace(/[/\-\\^$*+?.()|[\]{}]/g, '\\$&'),
-                $options: 'i'
-            }
+  getDataByName: publicProcedure.input(z.object({ name: z.string() })).query(async ({ ctx, input }) => {
+    const lol: ({ _id: { $oid: string } } & Prisma.HealthCenterGroupByOutputType)[] = await db.healthCenter.findRaw({
+      filter: {
+        names: {
+          // prisma doesnt have an elegant way of filtering if a letter occurs in a string[] so we use regexes.
+          $regex: input.name.replace(/[/\-\\^$*+?.()|[\]{}]/g, '\\$&'),
+          $options: 'i'
         }
+      }
     }) as unknown as [];
-    const format:IDToMeta = {};
-    for(const place of lol)
-      format[place._id.$oid] = { names: place.names, address: place.address }; 
+    const format: IDToMeta = {};
+    for (const place of lol)
+      format[place._id.$oid] = { names: place.names, address: place.address };
     return format;
   }),
   getAllProcedureTypes: publicProcedure.query(async () => {
     const t = await db.procedureType.findMany();
-    return t.filter((item,index,self) => self.findIndex(e=>item.name === e.name) === index);
+    return t.filter((item, index, self) => self.findIndex(e => item.name === e.name) === index);
   }),
 });
 

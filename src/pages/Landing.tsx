e921ee723da1ae3963238d14314d372022589faa
shringo <143767857+shringo@users.noreply.github.com>
2023-11-10 11:12:04 -1000

    adjust font size on landing

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -18,9 +18,9 @@ export default function searchPage() {
                         <Link href="/Search" className="mb-11 normal-case mt-6 btn w-40 h-14 text-xl bg-light-green hover:bg-hover-green text-green-gray border-0">Start Now</Link>
                     </div>
                 </div>
-                <div className="flex flex-co mb-10 pt-14">
+                <div className="flex flex-co mb-10 pt-7 sm:pt-14">
                     <div className="px-5 sm:px-0 sm:pl-60 sm:pr-2 justify-center">
-                        <h1 className="text-3xl  font-semibold text-center sm:text-left">Finding a Clinic</h1>
+                        <h1 className="text-4xl  font-semibold text-center sm:text-left">Finding a Clinic</h1>
                         <p className="text-2xl  font-semibold pt-4 text-dark-blue">1) Search by Clinic Name or Procedure Type</p>
                         <p className="text-xl  font-medium pt-2 pl-10">Use the search bar above and enter a clinic name or the type of procedure</p>
                         <p className="text-2xl  font-semibold pt-4 text-dark-blue">2) Check Information</p>
@@ -28,7 +28,7 @@ export default function searchPage() {
                         <p className="text-2xl  font-semibold pt-4 text-dark-blue">3) Contact Clinic</p>
                         <p className="text-xl  font-medium pt-2 pl-10">Use the listed information to learn more about the clinic and to contact them directly</p>
 
-                        <h1 className="text-3xl  font-semibold pt-8 text-center sm:text-left">What is Med-QUEST?</h1>
+                        <h1 className="text-4xl  font-semibold pt-8 text-center sm:text-left">What is Med-QUEST?</h1>
                         <p className="text-xl  font-medium pt-4">Med-QUEST is a government health insurance plan for the uninsured. QUEST covers a wide variety of healthcare services, so check if you may qualify for these benefits by checking</p>
                         <div className="flex justify-center">
                             <a className="normal-case btn  text-lg font-semibold mt-6 h-14 bg-light-green hover:bg-hover-green text-green-gray border-0" target="_blank" href="https://medquest.hawaii.gov/en/contact-us.html">Med-QUEST Eligibilty</a>
